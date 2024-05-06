df8fdc9b80cf355767833d1ceef8d9d6320d6eea
Kethelen Parra <kethelenvictoria2016@gmail.com>
2024-05-06 20:55:14 -0300

    feat: add promo banner to homepage

--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -7,7 +7,7 @@ const CategoryList = async () => {
     const categories = await db.category.findMany({});
 
     return (
-        <div className="flex grid-cold-2 gap-3">
+        <div className="grid grid-cols-2 gap-3">
             {categories.map((category) => (
                 <CategoryItem key={category.id} category={category} />
             ))}  
