3f3fc6fb860cad8de27f78f8b24d55158e966e87
Kethelen Parra <kethelenvictoria2016@gmail.com>
2024-05-06 23:30:03 -0300

    fix: update product itens

--- app/_components/product-item.tsx
+++ app/_components/product-item.tsx
@@ -3,13 +3,13 @@ import { Prisma } from "@prisma/client";
 import { calculateProductTotalPrice, formatCurrency } from "./_helpers/price";
 import { ArrowDownIcon } from "lucide-react";
 
-interface ProductItemProps{
+interface ProductItemProps {
     product: Prisma.ProductGetPayload<{
         include: {
             restaurant: {
-               select: {
-                name: true;
-               };     
+                select: {
+                    name: true;
+                };
             };
         };
     }>;
@@ -17,12 +17,12 @@ interface ProductItemProps{
 
 const ProductItem = ({ product }: ProductItemProps) => {
     return (
-        <div className="h-[150px] min-w-[150px] space-y-2">
+        <div className="flex flex-col">
             <div className="relative h-[150px] w-full">
-                <Image 
-                    src={product.imageUrl} 
-                    alt={product.name} 
-                    fill 
+                <Image
+                    src={product.imageUrl}
+                    alt={product.name}
+                    fill
                     className="rounded-lg object-cover shadow-md"
                 />
                 {product.discountPercentage && (
@@ -33,19 +33,21 @@ const ProductItem = ({ product }: ProductItemProps) => {
                         </span>
                     </div>
                 )}
-                
+
             </div>
-            <div>
-                <h2 className="truncate text-sm">{product.name}</h2>
-                <div className="flex items-center gap-1">
-                    <h3 className="font-semibold">
-                        {formatCurrency(calculateProductTotalPrice(product))}
-                    </h3>
-                    {product.discountPercentage > 0 && (
-                        <span className="text-xs text-muted-foreground line-through">
-                           {formatCurrency(Number(product.price))}
-                        </span>
-                    )}
+            <div className="flex flex-col justify-between flex-grow p-2">
+                <div>
+                    <h2 className="truncate text-sm">{product.name}</h2>
+                    <div className="flex items-center gap-1">
+                        <h3 className="font-semibold">
+                            {formatCurrency(calculateProductTotalPrice(product))}
+                        </h3>
+                        {product.discountPercentage > 0 && (
+                            <span className="text-xs text-muted-foreground line-through">
+                                {formatCurrency(Number(product.price))}
+                            </span>
+                        )}
+                    </div>
                 </div>
                 <div>
                     <span className="block text-xs text-muted-foreground">{product.restaurant.name}</span>
@@ -54,5 +56,5 @@ const ProductItem = ({ product }: ProductItemProps) => {
         </div>
     );
 }
- 
+
 export default ProductItem;
