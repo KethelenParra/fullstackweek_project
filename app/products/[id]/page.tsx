a5c195c6f838142efa7602e042ca64019d7a95f1
Kethelen Parra <kethelenvictoria2016@gmail.com>
2024-05-15 22:32:51 -0300

    feat: add sizes property to images

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,49 +1,52 @@
 import { db } from "@/app/_lib/prisma";
 import { notFound } from "next/navigation";
 import ProductImage from "./_components/product-image";
-import ProductDetails from "./_components/Product-details";
+import ProductDetails from "./_components/product-details";
+
 
 interface ProductPageProps {
-    params: {
-        id: string;
-    };
+  params: {
+    id: string;
+  };
 }
 
 const ProductPage = async ({ params: { id } }: ProductPageProps) => {
-    const product = await db.product.findUnique({
-        where: {
-            id,
-        },
-        include: {
-            restaurant: true,
-        },
-    });
+  const product = await db.product.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      restaurant: true,
+    },
+  });
 
-    if(!product){
-        return notFound();
-    }
+  if (!product) {
+    return notFound();
+  }
 
-    const juices = await db.product.findMany({
-        where: {
-            category: {
-                name: 'Sucos',
-            },
-            restaurant: {
-                id: product?.restaurant.id,
-            },
-        },
-        include: {
-            restaurant: true,
-        },
-    });
+  const juices = await db.product.findMany({
+    where: {
+      category: {
+        name: "Sucos",
+      },
+      restaurant: {
+        id: product?.restaurant.id,
+      },
+    },
+    include: {
+      restaurant: true,
+    },
+  });
 
-    return <div>
-        {/* IMAGE*/}
-       < ProductImage product={product}/>
+  return (
+    <div>
+      {/* IMAGEM */}
+      <ProductImage product={product} />
 
-        {/* TITULO e PREÇO*/}
-        <ProductDetails product={product} complementaryProducts={juices}/>
-    </div>;
-}
- 
+      {/* TITULO E PREÇO */}
+      <ProductDetails product={product} complementaryProducts={juices} />
+    </div>
+  );
+};
+
 export default ProductPage;
