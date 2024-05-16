7d511609144d4e3b3d325eb782ce5a7777908026
Kethelen Parra <kethelenvictoria2016@gmail.com>
2024-05-15 22:24:38 -0300

    feat: ensure restaurant can be favorited

--- app/_components/restaurant-list.tsx
+++ app/_components/restaurant-list.tsx
@@ -1,16 +1,29 @@
+import { getServerSession } from "next-auth";
 import { db } from "../_lib/prisma";
-import RestaurantItem from "./Restaurant-item";
+import { authOptions } from "../_lib/auth";
+import RestaurantItem from "./restaurant-item";
 
-const RestaurantList = async () => {  
 
-    const restaurants = await db.restaurant.findMany({ take: 10 });
-    return (
-      <div className="flex gap-4 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
-        {restaurants.map((restaurant) => (
-          <RestaurantItem key={restaurant.id} restaurant={restaurant}/>
-        ))}
-      </div>
-    );
-  };
- 
+const RestaurantList = async () => {
+  const session = await getServerSession(authOptions);
+
+  // TODO: pegar restaurantes com maior número de pedidos
+  const restaurants = await db.restaurant.findMany({ take: 10 });
+  const userFavoriteRestaurants = await db.userFavoriteRestaurant.findMany({
+    where: { userId: session?.user?.id },
+  });
+
+  return (
+    <div className="flex gap-4 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
+      {restaurants.map((restaurant) => (
+        <RestaurantItem
+          key={restaurant.id}
+          restaurant={restaurant}
+          userFavoriteRestaurants={userFavoriteRestaurants}
+        />
+      ))}
+    </div>
+  );
+};
+
 export default RestaurantList;
